40de95358e2db24aa36c487bdf84bd0a140a82dc
Victor Widell <victor@topmost.se>
2021-09-19 19:47:28 +0200

    inprog function arguments in variables.

--- circular-buffer.asm
+++ circular-buffer.asm
@@ -34,12 +34,21 @@
 	+stax .address
 }
 
-
+circularBufferGetIteratorNext_iterator
+	!word $00
+circularBufferGetIteratorNext_return
+	!byte $00
 !macro circularBufferGetIteratorNext .buffer, .iterator, .return{
+	; Transfer args to variables at once.
+	+ldax .iterator
+	+stax circularBufferGetIteratorNext_iterator
+	+ldax .return
+	+stax circularBufferGetIteratorNext_return
+
 	; Read the value to return.
 	ldy #0
-	lda (.iterator), y
-	sta .return
+	lda (circularBufferGetIteratorNext_iterator), y
+	sta circularBufferGetIteratorNext_return
 
 	; Find end pointer. (One step past the last element.)
 	+circularBufferGetIterator .buffer, $10
@@ -49,13 +58,13 @@
 	; Advance iterator.
 	lda #1
 	sta $06
-	+add16_8 .iterator, $06
+	+add16_8 circularBufferGetIteratorNext_iterator, $06
 
-	+cmp16 .iterator, $10
+	+cmp16 circularBufferGetIteratorNext_iterator, $10
 	bmi +
 	; Out of range, so return null pointer.
 	+ldaxImmediate $0000
-	+stax .iterator
+	+stax circularBufferGetIteratorNext_iterator
 +
 }
 
