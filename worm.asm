1bab4cb00f89dfeaf20e883338cabdc3a98400f4
Victor Widell <victor@topmost.se>
2021-09-26 13:47:52 +0200

    Use zpallocator in worm.

--- worm.asm
+++ worm.asm
@@ -1,3 +1,6 @@
+#import "zpallocator.asm"
+
+
 .macro wormAllocate () {
 	.byte $aa // length
 	.word $bbcc // pos, index into screen array
@@ -68,28 +71,33 @@
 table: .word 1, 40, -1, -40
 !:
 	// Find the offset in the table, by the direction.
-	wormGetDirection(worm, $02)
-	rol $02 // Multiply by 2, because words, not bytes.
-	ldx $02
+	.var _02 = allocateZpWord()
+	wormGetDirection(worm, _02)
+	rol _02 // Multiply by 2, because words, not bytes.
+	ldx _02
 	lda table, x
-	sta $02
+	sta _02
 	lda table+1, x
-	sta $03
+	sta _02+1
 
 	// Move the head.
-	wormGetPosition(worm, $04)
-	add16($04, $02)
-	wormSetPosition(worm, $04)
+	.var _04 = allocateZpWord()
+	wormGetPosition(worm, _04)
+	add16(_04, _02)
+	wormSetPosition(worm, _04)
 
 	// Grow if too short.
-	wormGetLength(worm, $02)
-	wormGetWantedLength(worm, $04)
-	lda $02
-	cmp $04
+	wormGetLength(worm, _02)
+	wormGetWantedLength(worm, _04)
+	lda _02
+	cmp _04
 	bpl !+
-	inc $02
-	wormSetLength(worm, $02)
+	inc _02
+	wormSetLength(worm, _02)
 !:
+
+	.eval deallocateZpWord(_02)
+	.eval deallocateZpWord(_04)
 }
 
 
@@ -99,7 +107,9 @@ table: .word 1, 40, -1, -40
 
 
 .macro wormGrowTail (worm) {
-	wormGetWantedLength(worm, $02)
-	inc $02
-	wormSetWantedLength(worm, $02)
+	.var _02 = allocateZpByte()
+	wormGetWantedLength(worm, _02)
+	inc _02
+	wormSetWantedLength(worm, _02)
+	.eval deallocateZpByte(_02)
 }
